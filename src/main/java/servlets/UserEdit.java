3a3edcb47ec478d073edfa98e98590077e0bbd1c
k0r0tk0ff <korotkov34reg@gmail.com>
2017-02-04 23:07:50 +0300

    add comment to UserEdit.java class

--- src/main/java/servlets/UserEdit.java
+++ src/main/java/servlets/UserEdit.java
@@ -9,8 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by user on 2/1/2017.
+
+/**.
+ * Class describe a logic of edit user`s parameters
+ * @author k0r0tk0ff
+ * @author peterarsentev
+ * @since 04.02.2017
+ * @version 1.1
  */
 
 public class UserEdit extends HttpServlet {
@@ -19,27 +24,48 @@ public class UserEdit extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        /**
+         * Bind parameter "hiddenId" for use in edit user operation.
+         */
         request.setAttribute("hiddenId", request.getParameter("id"));
 
+        /**
+         * Bind parameter "users" for access to database with users
+         */
         request.setAttribute("users", storage.getAll());
 
+        /**
+         * After binding - forward to jsp page with web-interface for enter
+         * new data of user.
+         */
         request.getRequestDispatcher("/WEB-INF/views/users/edit.jsp").forward(request, response);
 	}
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
+        /**.
+         * Delete user whith parameter "id" determinate in request
+         */
         storage.getInstance().delUserById(Integer.valueOf(request.getParameter("id")));
 
+        /**.
+         * Create new user with new parameter? but old parameter "id"
+         */
         User editedUser = new User();
         editedUser.setEmail(request.getParameter("newEmail"));
         editedUser.setLogin(request.getParameter("newLogin"));
         editedUser.setPassword(request.getParameter("newPassword"));
 
-        //Convert to Integer requested parameter "id"
-        // Remember! Servlets work whith only text-type parameters!
+        /**.
+         * Convert to Integer requested parameter "id"
+         * Remember! Servlets work whith only text-type parameters!
+         */
         editedUser.setId(Integer.valueOf(request.getParameter("id")));
 
+        /**.
+         * Save new user in our database.
+         */
         storage.getInstance().add(editedUser);
 
         response.sendRedirect(String.format("%s/",request.getContextPath()));
