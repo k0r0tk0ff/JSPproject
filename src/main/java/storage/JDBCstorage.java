db264103e8a675b5ff9776f6bc1bc62c3623b83e
k0r0tk0ff <korotkov34reg@gmail.com>
2017-04-01 20:21:14 +0300

    add some methods

--- src/main/java/storage/JDBCstorage.java
+++ src/main/java/storage/JDBCstorage.java
@@ -1,6 +1,9 @@
 package storage;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,13 +17,30 @@ import service.Settings;
  * Storage, where data is saved in DB
  */
 
-public class JDBCstorage implements Storage {
+public class JDBCstorage implements Storage throws ServletException,IOException {
+        
+    Connection connection = null;
 
     public JDBCstorage() {
         Settings settings = Settings.getInstance();
     }
 
+    final Connection connection=DriverManager.getConnection(
+        settings.getValue("jdbc.url"),
+        settings.getValue("jdbc.username"),
+        settings.getValue("jdbc.password"));
 
+    final Statement statement=connection.createStatement();
+
+    ResultSet resultSet=statement.executeQuery("SELECT * FROM clients");
+
+    while(resultSet.next()){
+        System.out.print("\n"+resultSet.getInt("id"));
+        System.out.print("            "+resultSet.getString("name"));
+        }
+        connection.close();
+    
+    
     @Override
     public void add(User user) {
 
@@ -43,7 +63,7 @@ public class JDBCstorage implements Storage {
 
     @Override
     public List<User> getAll() {
-        return null;
+                
     }
 
     @Override
