602f346a67731b84b6d4d9c503cfa80304c7ff83
k0r0tk0ff <korotkov34reg@gmail.com>
2017-04-02 18:39:01 +0300

    add some sql data

--- src/main/SQL/scheme.sql
+++ src/main/SQL/scheme.sql
@@ -3,7 +3,9 @@
 
 create TABLE clients (
  id serial PRIMARY KEY,
- name VARCHAR(20)
+ name VARCHAR(20),
+ password VARCHAR(20),
+ role VARCHAR(20)
 );
 
  /*
@@ -32,7 +34,28 @@ INSERT into pets (user_id, nick, type) VALUES ('1', 'PussyCat', 'cat');
  -- select colums from table client with alias client.
  -- alias may be use with join
 
+UPDATE clients as client set password = 'asdfgh123$'
+WHERE client.name = 'mpgonch1';
+
+UPDATE clients as client set email = 'kotiki@email.ru'
+WHERE client.name = 'mpgonch1';
+
+INSERT into clients (name, password, email, role) VALUES ('a','a','a@email.com','ROLE_ADMIN');
+
+INSERT into pets (user_id, nick, type) VALUES ('1', 'PussyCat', 'cat');
+
  -------------------------   SELECTING  &  Updating ---------------------------
+ -- show all in table clients
+
+SELECT * FROM public.clients
+ORDER BY id ASC;
+
+   -- add column
+
+ALTER TABLE clients ADD COLUMN role VARCHAR(20);
+ALTER TABLE clients ADD COLUMN password VARCHAR(20);
+ALTER TABLE clients ADD COLUMN email VARCHAR(20);
+
 
 SELECT * FROM clients as client;
 
@@ -55,11 +78,18 @@ on customer.id = pet.user_id;
 UPDATE pets as pet set pet.petId = '1'
 WHERE pet.nick = 'Tomcat';
 
+UPDATE clients as client set name = 'mpgonch1'
+WHERE client.name = 'Tomcat';
+
  -- update data
  -- in table pets find pet with name 'Tomcat' and rename to 'Dodya'
 
 UPDATE pets as pet set nick = 'Dodya'
 WHERE pet.nick = 'Tomcat';
+
+ -- set new value
+UPDATE clients set role = 'asdf'
+WHERE name = 'mpgonch1';
 
  -- delete
  -- if we want to delete client, previos we must delete his pets!!!
